8f0d144c381cb89418ac6c13ca8b026214792d34
liamkogan <103154727+liamkogan@users.noreply.github.com>
2023-01-22 09:21:09 +1100

    async pretty form

--- pages/api/mail.ts
+++ pages/api/mail.ts
@@ -1,6 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { StringLiteral } from "typescript";
 const sgMail = require("@sendgrid/mail");
 const key = process.env.SENDGRID_KEY;
 
