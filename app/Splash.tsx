63017756e4a5c2f645cfdfefea1c28ae22a9329f
liamkogan <103154727+liamkogan@users.noreply.github.com>
2023-01-31 21:16:24 +1100

    intro change

--- app/Splash.tsx
+++ app/Splash.tsx
@@ -8,11 +8,10 @@ function Splash() {
   return (
     <div className="text-center py-8 md:py-0  md:min-h-[min(calc(100vh-100px),800px)]  flex flex-col items-center justify-center ">
       <h1 className="font-black text-4xl md:text-6xl mb-4">
-        Digital Design & Web Development
+        Design & Development
       </h1>
       <p className="text-xl mb-6">
-        Designing & building user focussed, practical web solutions for engaging
-        online experiences. Sometimes just for fun.
+        Using my creative experience to build fun things on the web.
       </p>
       <div className="grid grid-cols-12 grid-rows-6 gap-4 items-center max-w-[min(700px,100%)] ">
         <div className="col-start-1 col-end-11 row-start-2 row-end-6  sm:col-end-11 sm:row-start-1 sm:row-end-6 my-4">
